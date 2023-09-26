c70ce36073f03da10535d2d59af8c8dc18536291
VannyTep <89964001+VannyTep@users.noreply.github.com>
2023-09-26 20:05:18 +0700

    Completed Smooth Player Controller

--- Scripts/Entities/Player_Movement.gd
+++ Scripts/Entities/Player_Movement.gd
@@ -4,20 +4,36 @@ extends CharacterBody2D
 
 
 @export var SPEED : float = 250.0
-@export var JUMP_VELOCITY : float = -400.0
+@export var JUMP_VELOCITY = 400.0
+@export var FALLMUTIPLIER : float = -2.5
+@export var LOWJUMPMUTIPLIER : float = -2.0
+
+@export var cayote_time : int = 15
+var cayote_counter : int = 0
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 func _physics_process(delta):
-	# Add the gravity.
+	
+	if is_on_floor():
+		cayote_counter = cayote_time # Set the cayate_counter is equal to cayote_time
+	
 	if not is_on_floor():
-		velocity.y += gravity * delta
-
+		velocity.y += gravity * delta  # Add the gravity.
+		
+		# check the cayote counter
+		if cayote_counter > 0:
+			cayote_counter -= 1
+		
 	# Handle Jump.
-	if Input.is_action_just_pressed("Jump") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-		Animator.play("Jump")
+	Jump(delta)
+	
+	# Handle Better Jump
+	if velocity.y < 0:
+		velocity += Vector2.UP * gravity * (FALLMUTIPLIER + 1) * delta
+	elif velocity.y > 0 and not Input.is_action_pressed("Jump"):
+		velocity += Vector2.UP * gravity * (LOWJUMPMUTIPLIER + 1) * delta
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
@@ -29,6 +45,7 @@ func _physics_process(delta):
 	elif direction == -1:
 		get_node("AnimatedSprite2D").flip_h = true
 	
+	# Handle the animations and movement
 	if direction:
 		velocity.x = direction * SPEED
 		
@@ -45,3 +62,12 @@ func _physics_process(delta):
 	
 	move_and_slide()
 
+func Jump(delta : float) -> void:
+	if Input.is_action_just_pressed("Jump") and cayote_counter > 0:
+		velocity.y = -JUMP_VELOCITY
+		Animator.play("Jump")
+		cayote_counter = 0
+		
+	if Input.is_action_just_released("Jump"):
+		if velocity.y < 0:
+			velocity = Vector2.DOWN * gravity * (FALLMUTIPLIER + 1) * delta
